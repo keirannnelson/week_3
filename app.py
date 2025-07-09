fb9096cf44444e50638acf71148d00104658cadd
Keiran Nelson <keirannnelson@gmail.com>
2025-07-09 16:41:21 +0000

    Full web page

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 import git
-from flask import Flask, request
+from flask import Flask, render_template, url_for, flash, redirect, request
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
+
 app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = '830edfd2958ae36b57b3c4d2bb2ac783'
 
 @app.route("/")                          # this tells you the URL the method below is related to
 def hello_world():
-    return "<h1> Cosmetic Change </h1> <p>Hello, World!</p>"        # this prints HTML to the webpage
+  return render_template('home.html', subtitle='Home page', text='This is the home page')       # this prints HTML to the webpage
   
+@app.route("/second_page")
+def second_page():
+  return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+ 
 @app.route("/update_server", methods=['POST'])
 def webhook():
     if request.method == 'POST':
